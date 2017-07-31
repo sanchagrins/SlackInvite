474e430cfa633c9fd1320ff915cf59f12ff12f47
sanchagrins <sanchagrins@gmail.com>
2017-07-31 10:08:45 +0900

    Fixed slack invite, added config file support

---
+++ slack_invite/slack_invite/config.py
@@ -0,0 +1,21 @@
+# ----------------------
+# Configuration file for the Django-SlackInvite App. Set your community name and
+# slack url here.
+# ----------------------
+
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+# SECURITY WARNING: Keep you slack token private in production
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+slack_token = get_env_variable('SLACK_TOKEN')
+
+# Set your community name and slack_url below
+community = 'Team-Name-Config'
+slack_url = 'django-slackinvite.slack.com'
--- slack_invite/slack_invite/views.py
+++ slack_invite/slack_invite/views.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
+import requests
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .forms import InviteForm
+from . import config 
 
 def home(request):
     if request.method == 'POST':
         form = InviteForm(request.POST)
         if form.is_valid():
+            email = form.cleaned_data['email_addr']
+            url = 'https://' + config.slack_url + '/api/users.admin.invite'
+            form = {'email' : email, 'token' : config.slack_token,
+                    'set_active' : 'true'}
+            r = requests.post(url, data=form)
             return HttpResponseRedirect('/thanks')
     else:
         form = InviteForm()
