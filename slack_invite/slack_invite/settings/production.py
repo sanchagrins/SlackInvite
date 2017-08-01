8c252dd6b9f0031b77770071fcd4a371084114f9
sanchagrins <sanchagrins@gmail.com>
2017-08-01 22:03:39 +0900

    Configured whitenoise for production

--- slack_invite/slack_invite/settings/production.py
+++ slack_invite/slack_invite/settings/production.py
@@ -2,3 +2,22 @@
 from .base import *
 
 DEBUG = False
+ALLOWED_HOSTS = ['localhost']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+            os.path.join(BASE_DIR, 'static'),
+            )
+MIDDLEWARE = [
+            'django.middleware.security.SecurityMiddleware',
+            'whitenoise.middleware.WhiteNoiseMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            ]
