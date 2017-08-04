7c027b1afa553e52d9252cf8fac7d08957f243ac
sanchagrins <sanchagrins@gmail.com>
2017-08-04 23:26:48 +0900

    UX/UI redesign

--- slack_invite/slack_invite/forms.py
+++ slack_invite/slack_invite/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class InviteForm(forms.Form):
-    email_addr = forms.EmailField(widget=forms.EmailInput({'placeholder':'Email address'}), label='')
+    email_addr = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-signin input','placeholder':'Email address'}), label='')
